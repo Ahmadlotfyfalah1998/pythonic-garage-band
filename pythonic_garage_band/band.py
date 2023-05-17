b65255d1ba364f3fdee18999a5f6761e7f4f34cb
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2023-05-17 12:08:13 +0000

    lab04-fixed

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,7 +1,11 @@
 
 
 class Musician :
- 
+   '''
+   in this class we create some function and method
+   to handle some functionality to the childs classes like 
+   __str__ and __repr__ and get instrument 
+   '''
    def __init__(self,name) :
       self.name = name
       
@@ -17,21 +21,39 @@ class Musician :
     
     
 class Guitarist(Musician):
+   """
+   this is a guitarist class from musician parent class 
+   which have a gitar as instrument
+
+  
+   """
    type ='guitar'
    type2='Guitarist'
-  
+   
    def play_solo(self):
      return 'face melting guitar solo'
    
 
+
+
 class Bassist(Musician):
+    """ 
+    this is a Bassist class from musician parent class
+    which have a gitar as instrument
+    """
     type ='bass'
     type2='Bassist'
     
     def play_solo(self):
      return 'bom bom buh bom'
 
+
+
 class Drummer(Musician):
+    """
+    this is a drummer class from musician parent class 
+    which have a drums as instrument 
+    """
     type ='drums'
     type2='Drummer'
     
@@ -39,26 +61,51 @@ class Drummer(Musician):
      return 'rattle boom crash'
     
     
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     
 class Band():
+   """
+    this band class will create some methods and functions for members
+    
+   """
+   instances = []
+
    def __init__(self,name,members) :
       self.name = name
       self.members = members
-    
+      Band.instances.append(self)
+
       
    def play_solos(self):
     member_list=[]
     for i in self.members:
       member_list.append(i.play_solo())
       
-    return member_list
-  
-  
-  
-  
-   def to_list(self):
       
-     return self.ininstances
+   
+ 
+    return member_list
+ 
+   @classmethod
+   
+   def to_list(cls):
+        return cls.instances
+ 
+   
+       
+  
+  
+  
+  
+  
 
 
 
@@ -73,4 +120,4 @@ class Band():
 
 if __name__ == '__main__':
     print(1)
-    
+malek=Guitarist("malik")
